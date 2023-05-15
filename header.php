53b7b7230477f213ecd6eea65dadd1b511bd0add
Markkeesi <tmarkkan@edu.lapinamk.fi>
2023-05-15 23:39:28 +0300

    pitas toimia

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
+<?php require "conn.php" ?>
 <?php
-session_start();
-
+  session_start();
 ?>
 
 <!DOCTYPE html>
@@ -9,44 +9,69 @@ session_start();
   <title>Kiinteistöhuolto R.Autio</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
 
 <div class="jumbotron text-center tausta" style="margin-bottom:0">
-  <h1 class="otsikko">Kiinteistöhuolto R.Autio</h1>
+  <h1 class="headtitle otsikko">Kiinteistöhuolto R.Autio</h1>
+  <!-- <p>Laadukasta ja nopeaa palvelua jo vuodesta 1996</p>  -->
 </div>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="../kiinteistohuolto3/index.php">Etusivu</a>
+  <a class="navbar-brand" href="index.html"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/referenssit.php">Referenssit</a>
+        <a class="nav-link" href="referenssit.php">Referenssit</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/tiedot.php">Yhteystiedot</a>
+        <a class="nav-link" href="tiedot.html">Yhteystiedot</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/lomake.php">Ota yhteyttä</a>
+        <a class="nav-link" href="lomake.html">Ota yhteyttä</a>
       </li>
+      <?php if(!isset($_SESSION['kayttaja_nimi']) & !isset($_SESSION['tyontekija_nimi'])): ?>
       <div class="dropdown">
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
           Kirjaudu sisään
         </button>
         <div class="dropdown-menu">
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginAJA.php">Asukkaat ja asiakkaat</a>
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginISAN.php">Isännöitsijät</a>
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginTyon.php">Työntekijät</a>
+          <a class="dropdown-item" href="loginAJA.php">Asukkaat ja asiakkaat</a>
+          <a class="dropdown-item" href="loginIsan.php">Isännöitsijät</a>
+          <a class="dropdown-item" href="loginTyon.php">Työntekijät</a>
         </div>
-      </div>    
+      </div>
+      <?php elseif(isset($_SESSION['tyontekija_nimi'])) : ?>
+        <div class="dropdown">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+          <?php echo $_SESSION['tyontekija_nimi']; ?>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="logout.php">Kirjaudu ulos</a>
+          <a class="dropdown-item" href="#">Omat tiedot</a>
+          <a class="dropdown-item" href="nakymat/VIKAindex.php">Katso valituksia</a>
+        </div>
+      </div>
+      <?php else : ?>
+        <div class="dropdown">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+          <?php echo $_SESSION['kayttaja_nimi']; ?>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="logout.php">Kirjaudu ulos</a>
+          <a class="dropdown-item" href="nakymat/nakyma.php">Tee vikailmoitus</a>
+        </div>
+      </div>
+      <?php endif; ?>    
     </ul>
   </div>  
 </nav>
--- index.php
+++ index.php
@@ -1,51 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Kiinteistöhuolto R.Autio</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php
 
-<div class="jumbotron text-center tausta" style="margin-bottom:0">
-  <h1 class="headtitle otsikko">Kiinteistöhuolto R.Autio</h1>
-  <!-- <p>Laadukasta ja nopeaa palvelua jo vuodesta 1996</p>  -->
-</div>
+  require "conn.php";
+  require "header.php";
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="../kiinteistohuolto3/index.php"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/referenssit.php">Referenssit</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/tiedot.php">Yhteystiedot</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../kiinteistohuolto3/lomake.php">Ota yhteyttä</a>
-      </li>
-      <div class="dropdown">
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-          Kirjaudu sisään
-        </button>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginAJA.php">Asukkaat ja asiakkaat</a>
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginISAN.php">Isännöitsijät</a>
-          <a class="dropdown-item" href="../kiinteistohuolto3/loginTyon.php">Työntekijät</a>
-        </div>
-      </div>    
-    </ul>
-  </div>  
-</nav>
+?>
 
 <div class="container" style="margin-top:30px; margin-left: 10px;">
   <div class="row">
@@ -72,9 +30,4 @@
   </div>
 </div><br><br>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Kiinteistöhuolto R.Autio            Y-tunnus: 789720-2</p>
-</div>
-
-</body>
-</html>
+<?php require "footer.php"; ?>
