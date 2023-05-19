557efc6f97dcf9e74846bda90d503d14206f4178
Markkeesi <tmarkkan@edu.lapinamk.fi>
2023-05-19 10:12:08 +0300

    däs

--- header.php
+++ header.php
@@ -24,11 +24,11 @@
 </div>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
   <a class="navbar-brand" href="index.php">Etusivu</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" href="referenssit.php">Referenssit</a>
